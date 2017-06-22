4a76f55e5805858e503f6a92ce0d23d1b2098640
GIGIGUN <sprewell20051116@gmail.com>
2017-06-22 17:19:27 +0800

    add comment as #

--- p10.py
+++ p10.py
@@ -1,22 +1,29 @@
 cast = ["Cleese", "Palin", 'Jones', "Idle"]
 print(cast)
+# Add the output of print
 
 print(len(cast))
+# Add the output of print
 
 print(cast[1])
+# Add the output of print
 
 cast.append("Gilliam")
 print(cast)
+# Add the output of print
 
 cast.pop()
-
 print(cast)
+# Add the output of print
 
 cast.extend(["Gillam", "Chapman"])
 print(cast)
+# Add the output of print
 
 cast.remove("Chapman")
 print(cast)
+# Add the output of print
 
 cast.insert(0, "Chapman")
 print(cast)
+# Add the output of print
